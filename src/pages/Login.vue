bf0a19d27a6b04d485c52cabeb7216da2acfd42f
alisaTofu <finnfinnlucky@gmail.com>
2024-07-01 11:28:02 +0700

    loginW

---
+++ src/pages/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fullscreen loginBg">login</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.loginBg {
+  background-color: #193657;
+}
+</style>
