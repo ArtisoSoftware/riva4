be234dcfc00bc2a8a549ff000e5eb07d798b6332
alisaTofu <finnfinnlucky@gmail.com>
2024-07-02 18:24:59 +0700

    year work

--- src/pages/Year.vue
+++ src/pages/Year.vue
@@ -8,7 +8,7 @@
           class="btn"
           icon="fa-solid fa-plus"
           no-caps
-          @click="AddNewYear()"
+          @click="AddNewYearBtn()"
           style="background-color: #2c4e96; color: white"
         />
       </div>
@@ -25,11 +25,45 @@
         </div>
       </div>
     </div>
+    <!-- Dialog add new year -->
+    <q-dialog v-model="isAddYear" persistent>
+      <div class="newYearDia">
+        <div class="headBar q-px-md">Add new year</div>
+
+        <div class="row justify-center q-pt-xl">
+          <div class="q-pt-sm">Year</div>
+          <div style="width: 25px"></div>
+          <div><q-input v-model="username" outlined dense /></div>
+        </div>
+        <div class="q-px-md row justify-center q-pt-lg">
+          <div>
+            <q-btn
+              label="Cancel"
+              no-caps
+              class="CancelBtn"
+              outline
+              @click="closeDia"
+              style="width: 120px"
+            />
+          </div>
+          <div style="width: 25px"></div>
+          <div>
+            <q-btn label="Save" no-caps class="CtaBtn" @click="saveNewStaff" />
+          </div>
+        </div>
+      </div>
+    </q-dialog>
   </div>
 </template>
 
 <script setup>
 import MenuSide from "../components/MenuSide.vue";
+import { ref } from "vue";
+// add New Year
+const isAddYear = ref(false);
+const AddNewYearBtn = () => {
+  isAddYear.value = true;
+};
 </script>
 
 <style lang="scss" scoped>
@@ -45,5 +79,16 @@ import MenuSide from "../components/MenuSide.vue";
 }
 .resultBlue {
   background-color: #e5ebf8;
+}
+
+.newYearDia {
+  width: 100%;
+  max-width: 450px;
+  height: 235px;
+  background-color: white;
+}
+.CtaBtn {
+  width: 120px;
+  background-color: #ffca4f;
 }
 </style>
