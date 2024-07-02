a51266047c6994ecd1ea79df6f5fa4a028d80da5
art <artisosoftware@gmail.com>
2024-07-02 08:13:01 +0700

    update

--- src/pages/server.js
+++ src/pages/server.js
@@ -1,6 +1,6 @@
 import { ref } from "vue";
 export function serverSetup() {
-  const serverData = ref("http://localhost/riva4/");
+  const serverData = ref("http://192.168.1.145/riva4/");
   return {
     serverData,
   };
